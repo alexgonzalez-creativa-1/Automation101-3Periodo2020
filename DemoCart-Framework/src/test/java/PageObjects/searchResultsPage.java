eb277b6aa59e764d835a25b95e6db52cb859a089
Alex Gonzalez <alex.gonzalez@ucreativa.com>
2020-10-20 17:16:17 -0600

    Clase 8 - Practica

---
+++ DemoCart-Framework/src/test/java/PageObjects/searchResultsPage.java
@@ -0,0 +1,11 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class searchResultsPage {
+
+    public homePage(WebDriver driver, String url) {
+        super(driver, url);
+    }
+
+}
